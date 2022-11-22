92b8acd6f8b9baea7d3a50a445a57c06cc609408
ColeTrammer <cole.trammer@gmail.com>
2022-11-22 15:00:08 -0800

    Add logic for comparing hand evaluations

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 
 #[derive(Clone, Copy, Debug)]
+#[repr(u8)]
 pub enum Suit {
     Hearts = 0,
     Diamonds = 1,
@@ -25,8 +26,8 @@ impl Suit {
 }
 
 #[derive(Clone, Copy, Debug)]
+#[repr(u8)]
 pub enum Number {
-    Ace = 1,
     Two = 2,
     Three = 3,
     Four = 4,
@@ -39,16 +40,16 @@ pub enum Number {
     Jack = 11,
     Queen = 12,
     King = 13,
+    Ace = 14,
 }
 
 impl Number {
     /// # Panics
     ///
-    /// Will panic if value is in the interval [1, 13]
+    /// Will panic if value is in the interval [2, 14]
     #[must_use]
     pub fn from_u8(value: u8) -> Self {
         match value {
-            1 => Self::Ace,
             2 => Self::Two,
             3 => Self::Three,
             4 => Self::Four,
@@ -61,8 +62,14 @@ impl Number {
             11 => Self::Jack,
             12 => Self::Queen,
             13 => Self::King,
+            14 => Self::Ace,
             _ => panic!("Invalid card number: {}", value),
         }
+    }
+
+    #[must_use]
+    pub const fn as_bit(self) -> u16 {
+        1 << (self as u8)
     }
 }
 
@@ -98,6 +105,104 @@ impl fmt::Debug for Card {
     }
 }
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u8)]
+pub enum HandKind {
+    StraightFlush = 8,
+    FourOfAKind = 7,
+    FullHouse = 6,
+    Flush = 5,
+    Straight = 4,
+    ThreeOfAKind = 3,
+    TwoPair = 2,
+    Pair = 1,
+    HighCard = 0,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct HandEvaluation {
+    kind: HandKind,
+    values: [u8; 3],
+}
+
+impl HandEvaluation {
+    #[must_use]
+    pub const fn new_straight_flush(high_card: Number) -> Self {
+        Self {
+            kind: HandKind::StraightFlush,
+            values: [high_card as u8, 0, 0],
+        }
+    }
+
+    #[must_use]
+    pub const fn new_four_of_a_kind(high_card: Number) -> Self {
+        Self {
+            kind: HandKind::FourOfAKind,
+            values: [high_card as u8, 0, 0],
+        }
+    }
+
+    #[must_use]
+    pub const fn new_full_house(high_card: Number, low_card: Number) -> Self {
+        Self {
+            kind: HandKind::FullHouse,
+            values: [high_card as u8, low_card as u8, 0],
+        }
+    }
+
+    #[must_use]
+    pub const fn new_flush(high_card: Number) -> Self {
+        Self {
+            kind: HandKind::Flush,
+            values: [high_card as u8, 0, 0],
+        }
+    }
+
+    #[must_use]
+    pub const fn new_straight(high_card: Number) -> Self {
+        Self {
+            kind: HandKind::Straight,
+            values: [high_card as u8, 0, 0],
+        }
+    }
+
+    #[must_use]
+    pub const fn new_three_of_a_kind(high_card: Number) -> Self {
+        Self {
+            kind: HandKind::ThreeOfAKind,
+            values: [high_card as u8, 0, 0],
+        }
+    }
+
+    #[must_use]
+    pub const fn new_two_pair(high_card: Number, low_card: Number, kicker: Number) -> Self {
+        Self {
+            kind: HandKind::TwoPair,
+            values: [high_card as u8, low_card as u8, kicker as u8],
+        }
+    }
+
+    #[must_use]
+    pub const fn new_pair(high_card: Number, kickers: u16) -> Self {
+        Self {
+            kind: HandKind::Pair,
+            values: [
+                high_card as u8,
+                (kickers >> 8) as u8,
+                (kickers & 0xFF) as u8,
+            ],
+        }
+    }
+
+    #[must_use]
+    pub const fn new_high_card(cards: u16) -> Self {
+        Self {
+            kind: HandKind::HighCard,
+            values: [(cards >> 8) as u8, (cards & 0xFF) as u8, 0],
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -108,5 +213,43 @@ mod tests {
             format!("{:?}", Card::new(Suit::Hearts, Number::Ace)),
             "Card { suit: Hearts, number: Ace }"
         )
+    }
+
+    #[test]
+    fn test_card_evaluations() {
+        let royal_flush = HandEvaluation::new_straight_flush(Number::Ace);
+        let straight_flush = HandEvaluation::new_straight_flush(Number::King);
+        let ace_ten_full_house = HandEvaluation::new_full_house(Number::Ace, Number::Ten);
+        let ace_nine_full_house = HandEvaluation::new_full_house(Number::Ace, Number::Nine);
+        let ace_pair_ten_kicker = HandEvaluation::new_pair(
+            Number::Ace,
+            Number::Ten.as_bit() | Number::Eight.as_bit() | Number::Seven.as_bit(),
+        );
+        let ace_pair_nine_kicker = HandEvaluation::new_pair(
+            Number::Ace,
+            Number::Nine.as_bit() | Number::Eight.as_bit() | Number::Seven.as_bit(),
+        );
+        let ace_high = HandEvaluation::new_high_card(
+            Number::Ace.as_bit()
+                | Number::Ten.as_bit()
+                | Number::Eight.as_bit()
+                | Number::Seven.as_bit()
+                | Number::Six.as_bit(),
+        );
+        let queen_high = HandEvaluation::new_high_card(
+            Number::Queen.as_bit()
+                | Number::Ten.as_bit()
+                | Number::Eight.as_bit()
+                | Number::Seven.as_bit()
+                | Number::Six.as_bit(),
+        );
+
+        assert!(royal_flush > straight_flush);
+        assert!(royal_flush > ace_ten_full_house);
+        assert!(ace_ten_full_house == ace_ten_full_house);
+        assert!(ace_ten_full_house > ace_nine_full_house);
+        assert!(ace_ten_full_house > ace_high);
+        assert!(ace_pair_ten_kicker > ace_pair_nine_kicker);
+        assert!(ace_high > queen_high);
     }
 }
